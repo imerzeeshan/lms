e765ac017b49cabd4d7d10693e88447ea162a945
Md Zeeshan <imerzeeshan@gmail.com>
2025-05-04 22:34:49 +0530

    LMS

---
+++ server/configs/mongodb.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+// Connet to the MongoDB DAtabase
+
+const connectDB = async () => {
+  mongoose.connection.on("connected", () =>
+    console.log("Database Connected Successfully")
+  );
+  await mongoose.connect(`${process.env.MONGODB_URI}/lms`);
+};
+
+export default connectDB;
